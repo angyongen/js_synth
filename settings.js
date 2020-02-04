c3222b8f6e8186f2db5fd32a7608e303e851a538
yongen <angyongen@gmail.com>
2020-02-05 00:22:35 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,30 +1,31 @@
-	var noteRepeats = 3 //the number of duplicate note audio stored
-	var appendToDocument = false //whether or not note audio is appended to the soundplayers div
-	var keyCodeMapping = [81, 50, 87, 51, 69, 82, 53, 84, 54, 89, 55, 85, 73, 57, 79, 48, 80, 219, 61, 221]
-	var sampleRate = 44100
-	var volume = 16384
+	var noteRepeats = 3; //the number of duplicate note audio stored
+	var appendToDocument = false; //whether or not note audio is appended to the soundplayers div
+	var keyCodeMapping = [81, 50, 87, 51, 69, 82, 53, 84, 54, 89, 55, 85, 73, 57, 79, 48, 80, 219, 61, 221];
+	var sampleRate = 44100;
+	var volume = 16384;
 
-	var totalTime1 = 0
-	var totalTimes1 = 0
-	var min1 = 99999
-	var max1 = 0
+	var totalTime1 = 0;
+	var totalTimes1 = 0;
+	var min1 = 99999;
+	var max1 = 0;
 
-	var totalTime2 = 0
-	var totalTimes2 = 0
-	var min2 = 99999
-	var max2 = 0
+	var totalTime2 = 0;
+	var totalTimes2 = 0;
+	var min2 = 99999;
+	var max2 = 0;
 
 	var audioCtx = new AudioContext();
 
-	var base64mode = false
+	var base64mode = false;
 
-	var soundplayersstorages = [[], [], [], []]
-	var soundplayersstorage
-	var soundchoice
-	var time
+	var soundplayersstorages;// = [[], [], [], []];
+	var soundplayersstorage;
+	var soundchoice;
+	var time;
 
 	function updateSoundChoice() {
 		soundchoice = parseInt(document.form.soundchoice.value);
+		if (!soundplayersstorages) soundplayersstorages = []
 		soundplayersstorage = soundplayersstorages[soundchoice]
 	}
 	//var soundplayersplaying = new Uint8Array(128)
